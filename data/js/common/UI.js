08382b81a31291d3759cd75706e6b6c167932d80
Crow <86617628+Raven-soul@users.noreply.github.com>
2023-12-28 16:25:01 +0300

    UI для меню и отшлифовка окон

---
+++ data/js/common/UI.js
@@ -0,0 +1,12 @@
+function hideChangeChevron(hideTagClassNameWithoutId, chevronClassNameWithoutId, button) {
+    let id = button.getAttribute("id");
+    let hideTagClassName = "." + hideTagClassNameWithoutId + "-" + id;
+    let chevronClassName = "." + chevronClassNameWithoutId + "-" + id;
+    if ( $(hideTagClassName).css("display") == "none" ){
+        $(hideTagClassName).css("display","block");
+        $(chevronClassName).html( "<i class=\"fa-solid fa-chevron-down\"></i>" );
+    } else {
+        $(hideTagClassName).css("display","none");
+        $(chevronClassName).html( "<i class=\"fa-solid fa-chevron-left\"></i>" );
+    }
+}
